8efb74802215a3103b0b53a800c95e755f1d3b56
Andrzej Ołdak <andrzej.oldak@diag.pl>
2025-09-18 10:46:03 +0200

    Day 18: Queues and Stacks

---
+++ Day 18 Queues and Stacks/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Day_18_Queues_and_Stacks;
+
+class Solution {
+    Queue<char> queue = new();
+    Stack<char> stack = new();
+
+    static void Main(String[] args) {
+        // read the string s.
+        string s = Console.ReadLine();
+        
+        // create the Solution class object p.
+        Solution obj = new Solution();
+        
+        // push/enqueue all the characters of string s to stack.
+        foreach (char c in s) {
+            obj.pushCharacter(c);
+            obj.enqueueCharacter(c);
+        }
+        
+        bool isPalindrome = true;
+        
+        // pop the top character from stack.
+        // dequeue the first character from queue.
+        // compare both the characters.
+        for (int i = 0; i < s.Length / 2; i++) {
+            if (obj.popCharacter() != obj.dequeueCharacter()) {
+                isPalindrome = false;
+                
+                break;
+            }
+        }
+        
+        // finally print whether string s is palindrome or not.
+        if (isPalindrome) {
+            Console.Write("The word, {0}, is a palindrome.", s);
+        } else {
+            Console.Write("The word, {0}, is not a palindrome.", s);
+        }
+    }
+
+    private char dequeueCharacter()
+    {
+        return queue.Dequeue();
+    }
+
+    private char popCharacter()
+    {
+        return stack.Pop();
+    }
+
+    private void enqueueCharacter(char c)
+    {
+        queue.Enqueue(c);
+    }
+
+    private void pushCharacter(char c)
+    {
+        stack.Push(c);
+    }
+}
