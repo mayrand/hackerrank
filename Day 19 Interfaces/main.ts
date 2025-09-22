80f9815ee3a20754642a5ba7ea3f466273ea6cf8
Andrzej Ołdak <andrzej.oldak@diag.pl>
2025-09-22 10:13:25 +0200

    Day 19: Interfaces

---
+++ Day 19 Interfaces/main.ts
@@ -0,0 +1,49 @@
+﻿'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+function main() {
+    console.log('I implemented: AdvancedArithmetic')
+    let line: string;
+    let aa = new AdvancedArithmetic();
+    while(line=readLine())
+    {
+        let num = Number.parseInt(line);
+        console.log(aa.divisorSum(num));
+    }
+}
+
+interface IAdvancedArithmetic {
+    divisorSum(n: number):number;
+}
+
+class AdvancedArithmetic implements IAdvancedArithmetic
+{
+    divisorSum(n: number): number {
+        let sum: number = 1;
+        for(let i=2;i<=n;i++){
+            let x = n%i;
+            if(x==n || Number.isNaN(x)) break;
+            else if(x>0) continue;
+            sum+=i;
+        }
+        return sum;
+    }
+}
